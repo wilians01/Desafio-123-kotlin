97ae33dce16ca2554a7254208cc74c998f9b23bf
wilians01 <wilians01100@gmail.com>
2025-01-05 16:30:49 -0300

    Desafio-01 e 02

---
+++ desafio-123/src/Desafios/Desafio-01.kt
@@ -0,0 +1,9 @@
+package Desafios
+
+fun main() {
+    val nota1 = 7.8
+    val nota2 = 5.9
+    val media = (nota1 + nota2)/2
+
+    println("A média dás suas nota é $media")
+}
---
+++ desafio-123/src/Desafios/Desafio-02.kt
@@ -0,0 +1,12 @@
+package Desafios
+
+fun main() {
+    println("Digite seu nome: ")
+    val nome = readLine()
+    println("Digite sua idade:")
+    val idade = readLine()
+    println("Digite seu salário")
+    val salario = readLine()
+
+    println("Olá $nome, cara você tem $idade anos, legal!! \n Seu sálario é $salario")
+}
