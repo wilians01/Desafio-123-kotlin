9b5af89b5da6271819a05b6586935a4e4f74acb2
wilians01 <wilians01100@gmail.com>
2025-01-06 17:53:47 -0300

    Desafio-03 incompleto

---
+++ desafio-123/src/Desafios/Desafio-03.kt
@@ -0,0 +1,15 @@
+package Desafios
+
+fun main() {
+    println("Digite dois números que irão resultar em operações de matemática  !!!")
+    println("Digite o primeiro numero:")
+    val numero1 = readLine()
+    val n1 = numero1
+    println("Digite o segundo numero:")
+    val numero2 = readLine()
+    val n2 = numero2
+    println("A soma dós dois numeros é: ${n1 + n2} ")
+
+
+
+}
