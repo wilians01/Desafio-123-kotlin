1f602ba19c4765e9d3b3fac5c5b80c1f6595385d
wilians01 <wilians01100@gmail.com>
2025-01-08 16:19:38 -0300

    Desafio-03 Realizado com sucesso

--- desafio-123/src/Desafios/Desafio-03.kt
+++ desafio-123/src/Desafios/Desafio-03.kt
@@ -3,18 +3,28 @@ package Desafios
 fun main() {
     println("Digite dois números que irão resultar em operações de matemática  !!!")
     println("Digite o primeiro numero:")
-    val numero1 = readLine()
+    val numero1 = readLine()?.toDoubleOrNull()
 
     println("Digite o segundo numero:")
-    val numero2 = readLine()
-
-    var total = (numero1 + numero2)
+    val numero2 = readLine()?.toDoubleOrNull()
 
 
-    println("A soma dós dois numeros é: ${total} ")
-    println("A soma dós dois numeros é: ${total} ")
-    println("A soma dós dois numeros é: ${total} ")
-    println("A soma dós dois numeros é: ${total} ")
+
+    if (numero1 != null && numero2 != null ){
+        println("A soma dos dois numeros é: ${numero1 + numero2} ")
+        println("A Subtração dos dois numeros é: ${numero1 - numero2} ")
+        println("A multiplicação dos dois numeros é: ${numero1 * numero2} ")
+        if (numero2 != 0.0){
+            println("A divisão dos dois numeros é:${numero1/numero2}")
+        }else{
+            println("A divisão não é posssivl (divisãopor zero).")
+        }
+    }else{
+        println("Por favor, insira números validos! ")
+    }
+
+
+
 
 
 
