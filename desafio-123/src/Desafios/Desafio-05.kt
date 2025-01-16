603c1ed00df698acd1f63511527c2b8592099786
wilians01 <wilians01100@gmail.com>
2025-01-16 12:05:51 -0300

    Realização do Desafio-05

--- desafio-123/src/Desafios/Desafio-05.kt
+++ desafio-123/src/Desafios/Desafio-05.kt
@@ -1,2 +1,16 @@
 package Desafios
 
+fun main() {
+    // Entrada: custo de fábrica do automóvel
+    print("Digite o custo de fábrica do automóvel: R$ ")
+    val custoFabrica = readLine()?.toDoubleOrNull() ?: 0.0
+
+    // Cálculos
+    val porcentagemRevendedor = custoFabrica * 0.25
+    val impostos = custoFabrica * 0.45
+    val precoFinal = custoFabrica + porcentagemRevendedor + impostos
+
+    // Saída
+    println("O preço final do automóvel ao consumidor é: R$ %.2f".format(precoFinal))
+}
+
