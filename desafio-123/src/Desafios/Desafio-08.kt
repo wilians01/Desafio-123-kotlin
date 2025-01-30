58f7a56078ff51b0f203ee3fa4b460c922dda1a1
wilians01 <wilians01100@gmail.com>
2025-01-30 17:13:04 -0300

    Algumas atualizações

---
+++ desafio-123/src/Desafios/Desafio-08.kt
@@ -0,0 +1,16 @@
+package Desafios
+
+fun main() {
+    print("Digite o dia: ")
+    val dia = readLine()?.toIntOrNull()
+
+    print("Digite o mês: ")
+    val mes = readLine()?.toIntOrNull()
+
+    if (dia != null && mes != null && dia in 1..30 && mes in 1..12) {
+        val diasPassados = (mes - 1) * 30 + dia
+        println("Dias passados desde o início do ano: $diasPassados")
+    } else {
+        println("Entrada inválida. Certifique-se de inserir um dia entre 1 e 30 e um mês entre 1 e 12.")
+    }
+}
