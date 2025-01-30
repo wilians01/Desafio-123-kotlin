8fb585202472e68e8ff8142f8160b0f378970cf4
wilians01 <wilians01100@gmail.com>
2025-01-30 16:54:21 -0300

    Algumas atualizações

--- desafio-123/src/Desafios/Desafio-07.kt
+++ desafio-123/src/Desafios/Desafio-07.kt
@@ -17,4 +17,20 @@ fun main() {
     }
 }
 
+// Feito de outro maneira
+
+//fun main() {
+//    val precoPorQuilo = 14.00 // Preço por quilo em reais
+//
+//    print("Digite o peso do prato (em quilos): ")
+//    val peso = readLine()?.toDoubleOrNull()
+//
+//    if (peso != null && peso > 0) {
+//        val valorAPagar = peso * precoPorQuilo
+//        println("Valor a pagar: R$ %.2f".format(valorAPagar))
+//    } else {
+//        println("Peso inválido. Por favor, insira um valor válido.")
+//    }
+//}
+
 
